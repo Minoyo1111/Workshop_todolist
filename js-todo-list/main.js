33b086749e6ae9472d88a35151d2d58a734349e6
mino <oil_chen@cycu.org.tw>
2020-12-08 12:18:23 +0800

    done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,18 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", function(){
+  let to_do_list = document.querySelectorAll("li");
+
+  // console.log (to_do_list)
+  for (let to_do of to_do_list){
+    // console.log (to_do_list[i])
+    to_do.addEventListener("click", function(){
+      to_do.classList.toggle('checked');
+      // alert("hi")
+    })
+  }
+
+
+
+
+  })
+
